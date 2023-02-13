0870ae9a4db2fc6376f7e7b7f16bf92b65977b25
Mihai Florian <mflorian90@yahoo.com>
2023-02-13 19:37:19 +0200

    Spring initialization

---
+++ src/main/java/com/sda/OnlineShop/OnlineShopApplication.java
@@ -0,0 +1,13 @@
+package com.sda.OnlineShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sda/OnlineShop/OnlineShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sda.OnlineShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
