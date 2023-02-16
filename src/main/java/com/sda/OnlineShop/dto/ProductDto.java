381b81068c775ef7f32563e2d25bbe85c3b1c778
Mihai Florian <mflorian90@yahoo.com>
2023-02-16 21:00:06 +0200

    Added home page and adding img

--- src/main/java/com/sda/OnlineShop/dto/ProductDto.java
+++ src/main/java/com/sda/OnlineShop/dto/ProductDto.java
@@ -13,5 +13,7 @@ public class ProductDto {
     private String category;
     private String quantity;
     private String description;
+    @ToString.Exclude
+    private String image;
 
 }
--- src/main/java/com/sda/OnlineShop/service/ProductService.java
+++ src/main/java/com/sda/OnlineShop/service/ProductService.java
@@ -6,6 +6,10 @@ import com.sda.OnlineShop.mapper.ProductMapper;
 import com.sda.OnlineShop.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class ProductService {
@@ -14,9 +18,26 @@ public class ProductService {
     @Autowired
     private ProductRepository productRepository;
 
-    public void addProduct(ProductDto productDto) {
-        Product product = productMapper.map(productDto);
+    public void addProduct(ProductDto productDto, MultipartFile productImage) {
+
+        Product product = productMapper.map(productDto, productImage);
         productRepository.save(product);
+
+
+
     }
 
+    public List<ProductDto> getAllProductDtos() {
+        List<Product> products = productRepository.findAll();
+        List<ProductDto> productDtos = new ArrayList<>();
+        for (Product product : products) {
+            ProductDto productDto = productMapper.map(product);
+
+            productDtos.add(productDto);
+        }
+
+        return productDtos;
+
+
+    }
 }
